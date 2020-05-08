c7931c110a02f2263e1b46e0f48da894cd7703eb
kehanguo <52185318+kehanguo@users.noreply.github.com>
2020-05-08 10:43:11 -0500

    Update testemail.py

--- testemail.py
+++ testemail.py
@@ -1,51 +1,39 @@
-import smtplib
-import ssl 
-from picamera import PiCamera  
-from time import sleep  
-from email.mime.multipart import MIMEMultipart  
-from email.mime.base import MIMEBase  
-from email.mime.text import MIMEText  
-from email.utils import formatdate  
-from email import encoders  
-from smtplib import SMTPException  
-camera = PiCamera()  
-camera.resolution=(640,480)
- 
-camera.start_preview()  
-sleep(5)  
-camera.capture('/home/pi/image.jpg')     # image path set
-sleep(5)  
-camera.stop_preview()  
-def send_an_email():  
-    toaddr = 'email address you want to send'
-    me= 'your address'     
-    subject = "Senior design testing"              # Subject
-  
-    msg = MIMEMultipart()  
-    msg['Subject'] = subject  
-    msg['From'] = me
-    msg['To'] =toaddr  
-    msg.preamble = "test "   
-   msg.attach(MIMEText(text))  
-  
-    part = MIMEBase('application', "octet-stream")  
-    part.set_payload(open("image.jpg", "rb").read())  
-    encoders.encode_base64(part)  
-    part.add_header('Content-Disposition', 'attachment; filename="image.jpg"')   # File name and format name
-    msg.attach(part)  
-  
-     try: 
-       s = smtplib.SMTP('smtp.gmail.com', 587)  # Protocol
-       s.ehlo()  
-       s.starttls()  
-       s.ehlo()  
-       s.login(user = 'email address', password = 'password')  # User id & password
-       s.send_message(msg)  
-       s.sendmail(me, toaddr, msg.as_string())  
-       s.quit()  
-    #except:  
-    #   print ("Error: unable to send email")    
-    except SMTPException as error:  
-          print ("Error")                # Exception
-  
-send_an_email()
+import imaplib
+import email
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+   
+mail.login('email', 'password')
+   
+   
+mail.list() 
+mail.select('inbox') 
+result, data = mail.uid('search', None, "unseen")
+   
+i = len(data[0].split()) 
+for x in range(i):
+    latest_email_uid = data[0].split()[x] 
+    result, email_data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+   
+    raw_email = email_data[0][1]
+   
+    raw_email_string = raw_email.decode('utf-8')
+   
+    email_message = email.message_from_string(raw_email_string)
+   
+    for part in email_message.walk():
+        if part.get_content_type() == "text/plain": 
+            body = part.get_payload(decode=True)
+            messajbody = body.decode('utf-8')
+            messajbody = messajbody.split("<div dir=\"auto\">")
+            messajbody = messajbody[0].replace("\n","")
+            str_a= "yes"
+            str_b= "no"
+            
+            if messajbody == str_a:
+                print(str_b)
+                
+            if messajbody == str_b:
+                print(str_a)
+            else:
+                print(messajbody)
+            print(messajbody)
