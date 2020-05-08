49fe29258b9b3774da771893e1b963f9fea2e593
kehanguo <52185318+kehanguo@users.noreply.github.com>
2020-05-08 10:39:44 -0500

    Update email-receive.py

--- email-receive.py
+++ email-receive.py
@@ -1,47 +1,73 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import imaplib
-import email
-mail = imaplib.IMAP4_SSL('imap.gmail.com')
-   
-mail.login('username', 'password')
-   
-   
-mail.list() 
-mail.select('inbox') 
-result, data = mail.uid('search', None, "ALL")
-   
-i = len(data[0].split()) 
-for x in range(i):
-    latest_email_uid = data[0].split()[x] 
-    result, email_data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-   
-    raw_email = email_data[0][1]
-   
-    raw_email_string = raw_email.decode('utf-8')
-   
-    email_message = email.message_from_string(raw_email_string)
-   
-    for part in email_message.walk():
-        if part.get_content_type() == "text/plain": 
-            body = part.get_payload(decode=True)
-            messajbody = body.decode('utf-8')
-            messajbody = messajbody.split("<div dir=\"auto\">")
-            messajbody = messajbody[0].replace("\n","")
-            print(messajbody)
-
-
-# In[ ]:
+import smtplib
+import RPi.GPIO as IO
+from  picamera import PiCamera
+from email.mime.multipart import MIMEMultipart  
+from email.mime.base import MIMEBase  
+from email.mime.text import MIMEText  
+from email.utils import formatdate  
+from email import encoders  
+from smtplib import SMTPException  
+from  time import sleep
+camera= PiCamera()
+camera.vflip
 
 
 
+IO.setwarnings(False)
+IO.setmode (IO.BCM)
 
+IO.setup(22,IO.OUT)
+IO.setup(27,IO.OUT)
+IO.setup(23,IO.IN)
 
-# In[ ]:
+while 1:
+
+ if(IO.input(23)==True):
+    IO.output(27,True)
+    IO.output(22,False)
+    camera.start_preview()
+    sleep(5)
+    camera.vflip=True
+    camera.capture('/home/pi/image.jpg')
+    sleep(5)
+    camera.stop_preview()
+
+    fromaddr = "your email"
+    toaddr = "receiver's email"
+#subject=  "email a image through raspberry" 
+    msg = MIMEMultipart()
+ 
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "email an image through raspberry pi"
+     
+    body = "Door notification"
+     
+    msg.attach(MIMEText(body, 'plain'))
+     
+    filename = "raspberry image"
+    attachment = open("/home/pi/image.jpg", "rb")
+     
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition',' attachment; filename="image.jpg"')
+     
+    msg.attach(part)
+
+     
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr,'your password')
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+
+  
+if(IO.input(23)==False):
+    IO.output(22,True)
+    IO.output(27,False)
+
 
 
 
